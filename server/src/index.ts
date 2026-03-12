0140bdf5d2a30bbb14754fc922350b4bb7322e19
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-12 16:18:44 +0800

    added routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import todoRoutes from "./routes/todoRoutes"
 
 dotenv.config()
 
 const app = express()
-
 app.use(cors());
 app.use(express.json());
+app.use('/api/todos', todoRoutes)
 
 app.get("/", (req, res) => {
   res.statusCode = 200;
