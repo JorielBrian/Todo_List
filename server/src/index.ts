29059b800abbc9108e09f09ea8d815fa3ebe52e1
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-13 00:32:46 +0800

    added query endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,47 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { logger } from "./middleware/logger";
 import todoRoutes from "./routes/todoRoutes"
 
 dotenv.config()
 
 const app = express()
+
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(logger);
+
+// Routes
 app.use('/api/todos', todoRoutes)
 
+// Get all todos
 app.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "API running" })
+});
+
+// Create a new todo
+app.post("/", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "API running" })
+});
+
+// Update a todo
+app.put("/:id", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "API running" })
+});
+
+// Delete a todo
+app.delete("/:id", (req, res) => {
+  res.statusCode = 200;
+  res.json({ message: "API running" })
+});
+
+// Get a single todo
+app.get("/:id", (req, res) => {
   res.statusCode = 200;
   res.json({ message: "API running" })
 });
